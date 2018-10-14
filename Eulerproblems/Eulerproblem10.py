406ea2743d9c7e9b36b8c17d81e51feb405aafc6
Muthu Krishnan <muthukrishnan06@gmail.com>
2018-10-14 13:58:56 +0400

    Update Eulerproblem10.py
    Optmized performance

--- Eulerproblems/Eulerproblem10.py
+++ Eulerproblems/Eulerproblem10.py
@@ -1,22 +1,31 @@
-# Function to get the primefactorial 
+import time
+import math
+
+# Function to get the primefactorial
 def primesummation(number):
+    # Mark the performance of the algorithm
+    start = time.time()
     primestart = 2
     sumofprime = 0
     while primestart < number:
         if (getprime(primestart)):
             sumofprime = sumofprime + primestart
-        primestart+=1
+        primestart += 1
+    end = time.time()
+    print("time is", end-start)
     return sumofprime
-    
+
 # Function to get the prime number list
 def getprime(j):
     i = 1
-    while i<= j:
-        if i!= 1 and i!= j:
-            if j%i == 0:
+    # Looping the number till Square root reduces complexity to O(n * sqrt(n))
+    while i <= math.sqrt(j):
+        if i != 1 and i != j:
+            if j % i == 0:
                 return False
-        i+= 1
+        i += 1
     return True
+
 
 # Get the input number for which largest prime factor is required
 number = int(input("Enter the number:"))
