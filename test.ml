09279fe4d87fcf8dcd19aab802721dda8ab8af0b
Muthu Krishnan <muthukrishnan06@gmail.com>
2018-10-17 17:28:47 +0530

    Update test.ml

--- test.ml
+++ test.ml
@@ -13,6 +13,7 @@ let one_msg =
     
 let already_Voted = Int32 1
 let not_owner = Int32 2
+let txn_accepted = Int32 3
 let one = Uint128 1
     
 (***************************************************)
@@ -34,37 +35,41 @@ field voteCounts : Map Uint32 Uint256 = Emp ByStr20 Uint256
 
 (* Transition 1: Give Right to Vote *)
 transition giveRightToVote (voter: ByStr20)
-    isAuthorized = builtin eq owner _sender;
-    match isAuthorized with
-    | True =>
-        prs1 = builtin put voterWeight voter one;
-    | False => 
-        msg  = {_tag : "Not Owner"; _recipient : _sender; _amount : Uint128 0;
-              code : not_owner};
-        msgs = one_msg msg;
-        send msgs
-    end 
+  isAuthorized = builtin eq owner _sender;
+  match isAuthorized with
+  | True  => 
+    prs1  = builtin put voterWeight voter one;
+    msg   = {_tag : "Accepted"; _recipient : _sender; _amount : Uint128 0;
+            code : txn_accepted};
+    msgs  = one_msg msg;
+    send msgs 
+  | False => 
+    msg   = {_tag : "Not Owner"; _recipient : _sender; _amount : Uint128 0;
+            code : not_owner};
+    msgs  = one_msg msg;
+    send msgs
+  end 
 end
 
 (* Transition 1: Give Right to Vote *)
-transition vote (Proposal: Uint256)
-    isVoted = builtin get voted _sender;
-    match isVoted with
-    | True =>
-        msg  = {_tag : "Already Voted"; _recipient : _sender; _amount : Uint128 0;
-              code : already_Voted};
-        msgs = one_msg msg;
-        send msgs
-    end 
-    tmp1 = builtin lt Proposal numberofProp;
-    match tmp1 with
-    | False =>
-        builtin put Voted _sender True;
-        builtin put Votes _sender Proposal;
-        numofprop = builtin get voteCounts Proposal;
-        voteweight = builtin get voterWeight _sender;
-        Totalprop = builtin add numbofprop voteweight;
-        builtin put voteCounts Proposal TotalProp;
-    end     
+transition vote (proposal: Uint256)
+  isVoted = builtin get voted _sender;
+  match isVoted with
+  | True =>
+    msg  = {_tag : "Already Voted"; _recipient : _sender; _amount : Uint128 0;
+            code : already_Voted};
+    msgs = one_msg msg;
+    send msgs
+  end 
+  tmp1 = builtin lt proposal numberofProp;
+  match tmp1 with
+  | False =>
+    voted = builtin put Voted _sender True;
+    votes = builtin put Votes _sender proposal;
+    numofprop = builtin get voteCounts proposal;
+    voteweight = builtin get voterWeight _sender;
+    totalprop = builtin add numbofprop voteweight;
+    voutecounts = builtin put voteCounts proposal TotalProp;
+  end     
 end
 
